887d572a50e4bec0db6c8a85f853d179d43cb758
sonuprasad66 <sp.srp1999@gmail.com>
2023-03-26 20:18:32 +0530

    mcq

--- Yt-coding-1/int.js
+++ Yt-coding-1/int.js
@@ -30,14 +30,14 @@
 // OUTPUT:abcd
 
 // 5.Remove first character from the string
-let str = "Hello,How are you";
-let result = str.substring(1, str.length);
-console.log(result);
+// let str = "Hello,How are you";
+// let result = str.substring(1, str.length);
+// console.log(result);
 // OUTPUT: "ello,How are you";
 
 // 6.Remove last character from the string
-let str = "Hello,How are you";
-console.log(str.substring(0, str.length - 1));
+// let str = "Hello,How are you";
+// console.log(str.substring(0, str.length - 1));
 // OUTPUT: " Hello,How are yo";
 
 // 7.Whenever we compare two blank arrays, what will be the output?
